6f6e4c44f0604ca0479ac65d01097874871aa02a
nightnfun@yahoo.com <Chris344>
2017-11-28 14:45:54 +0100

    auth google template

--- server/config/auth.js
+++ server/config/auth.js
@@ -18,7 +18,6 @@ module.exports = {
   'googleAuth' : {
     'clientID'      : 'your-secret-clientID-here',
     'clientSecret'  : 'your-client-secret-here',
-
     'callbackURL'   : 'http://localhost:4200/auth/google/callback'
   }
 
